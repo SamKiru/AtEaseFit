53b1df7b6a75d83b79610bb85ae9b85aa1c4d2ff
LenJL <john09876lennon@gmail.com>
2025-02-17 11:23:58 +0530

    test

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -18,7 +18,7 @@ export default function Signin() {
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <h1  className="mx-auto h-10 w-auto">AtEaseFit</h1>
             <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white-900">
-              Sign in to your account
+              Sign in to your accountssssssssssssssssss
             </h2>
           </div>
   
